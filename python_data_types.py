1cfae0639e0c9ecb7432af4a74a8212b2c5feebb
AndreiHustiuc <andreihustiuc@yahoo.com>
2021-12-02 20:08:55 +0200

    Update python_data_types.py

--- python_data_types.py
+++ python_data_types.py
@@ -70,7 +70,107 @@ print(slice_fun(sample_string))
 sample_string = 'restart'
 
 for item in sample_string:
-    print(sample_string.count(item))
-    
+    if sample_string.count(item) >= 2:
+        res = sample_string[::-1].replace(item, '$', 1)
 
+print(res[::-1])
+
+#Write a Python program to get a single string from two given strings, separated by a space and swap the first two characters of each string
+sample_string = 'abc', 'xyz'
+
+print(sample_string[1][0:2]+sample_string[0][2:]+' '+sample_string[0][0:2]+sample_string[1][2:])
+
+
+#Write a Python program to add 'ing' at the end of a given string (length should be at least 3). If the given string is already ends with 'ing' then add 'ly' instead. If the string length of the given string is less than 3, leave it unchanged.
+
+sample_string = 'string'
+
+if len(sample_string) < 3:
+    print(sample_string)
+elif sample_string[-3:] == 'ing':
+    print(sample_string + 'ly')
+else:
+    print(sample_string + 'ing')
+
+
+#Write a Python function that takes a list of words and returns the length of the longest one.
+lst=["CRV", "Outback", "XC90", "GL", "Cherokee", "Escalade"]
+length=list()
+
+for item in lst:
+    length.append(len(item))
+    
+print(max(length))
+
+#Write a Python program to test whether an input is an integer.
+#num = input('Enter a number: ')
+#print(num == int())
+
+#Write a Python program to sort (ascending and descending) a dictionary by value
+dict = {'g': 2, 'o': 3, 'l': 1, 'e': 1, '.': 1, 'c': 1, 'm': 1}
+
+sorted_dict = sorted(dict.values())
+sorted_dict_rev = sorted(dict.values(), reverse=True)
+print(sorted_dict)
+print(sorted_dict_rev)
+
+#Write a Python program to sort (ascending and descending) a dictionary by key value
+dict = {'g': 2, 'o': 3, 'l': 1, 'e': 1, '.': 1, 'c': 1, 'm': 1}
+
+sorted_dict = sorted(dict)
+sorted_dict_rev = sorted(dict, reverse=True)
+print(sorted_dict)
+print(sorted_dict_rev)
+
+#Write a Python program to add key to a dictionary.
+sample_dictionary = {0:10, 1:20}
+
+sample_dictionary.update({2: 30})
+print(sample_dictionary)
+
+#Write a Python program to concatenate following dictionaries to create a new one.
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+
+new_dict = {**dic1, **dic2, **dic3}
+print(new_dict)
+
+#Write a Python program to check if a given key already exists in a dictionary.
+dict = {'g': 2, 'o': 3, 'l': 1, 'e': 1, '.': 1, 'c': 1, 'm': 1}
+
+def exist_in(dictionary, new_key):
+    for key in dictionary.keys():
+        if new_key == key:
+            return True
+        else:
+            return False
+
+print(exist_in(dict, 'g'))
+        
+#Write a Python program to iterate over dictionaries using for loops
+dict = {'g': 2, 'o': 3, 'l': 1, 'e': 1, '.': 1, 'c': 1, 'm': 1}
+
+for key, values in dict.items():
+    print(key, values)
+    
+#Write a Python program to remove duplicates from a list.
+lst=[55, 777, 54, 6, 76, 101, 1, 2, 8679, 123, 99, 54, 101]
+print(set(lst))
+    
+#Write a Python program to check a list is empty or not.
+lst = [1]
+
+def is_empty(list):
+    if len(list) == 0:
+        return True
+    else:
+        return False
+
+print(is_empty(lst))
+
+#Write a Python program to clone or copy a list.
+lst=[55, 777, 54, 6, 76, 101, 1, 2, 8679, 123, 99, 54, 101]
+new_lst = lst
+print(new_lst)
 
