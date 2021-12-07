2d97a5cfdebc91881d1c515e731cb3d8a1a0c41e
AndreiHustiuc <andreihustiuc@yahoo.com>
2021-12-07 14:16:20 +0200

    Update supp_home_work_2.py

--- supp_home_work_2.py
+++ supp_home_work_2.py
@@ -3,6 +3,7 @@ Find the longest word in a string, if there are more than one with same size, re
 """
 
 sample_string = 'Find the longest word in a string, if there are more than one with same size, return the first one'
+
 lst_string = sample_string.split(' ')
 len_words = []
 
@@ -10,3 +11,15 @@ for item in lst_string:
     len_words.append(len(item))
 
 print(lst_string[len_words.index(max(len_words))])
+
+#using dictionary
+dict_string = {}
+for item in lst_string:
+    dict_string.update({item: len(item)})
+    
+def get_key(val):
+    for key, value in dict_string.items():
+        if val == value:
+            return key
+        
+print(get_key(max(dict_string.values())))
