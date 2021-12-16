ab0a450fc2b3d9df9bf80ed5650f2d08b06b4457
AndreiHustiuc <andreihustiuc@yahoo.com>
2021-12-16 23:11:25 +0200

    update_git_hub

---
+++ src/home_work/supp_home_work_less_5_part1.py
@@ -0,0 +1,24 @@
+"""
+Write a program that prints the numbers from 1 to 100. 
+But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz. 
+For numbers which are multiples of both three and five print FizzBuzz.
+"""
+
+num_list = [i for i in range(1, 101)]
+
+for item in range(len(num_list)):
+    if num_list[item] % 15 == 0:
+        num_list[item] = 'FizzBuzz'    
+    elif num_list[item] % 3 == 0:
+        num_list[item] = 'Fizz'
+    elif num_list[item] % 5 == 0:
+        num_list[item] = 'Buzz'
+    
+    
+print(num_list)
+
+
+"""
+Write a function that will take a given string 175 and reverse the order of the words. 
+“Hello world” becomes “world Hello” and “May the Fourth be with you” becomes “you with be Fourth the May”
+"""
