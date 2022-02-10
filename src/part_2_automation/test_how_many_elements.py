85ae953e9ea3149bc824bc5e5c34c27ae9f5f7b1
AndreiHustiuc <andreihustiuc@yahoo.com>
2022-02-10 13:01:47 +0200

    Update test_how_many_elements.py

--- src/part_2_automation/test_how_many_elements.py
+++ src/part_2_automation/test_how_many_elements.py
@@ -2,42 +2,59 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-SEARCH_ELEMENT = 'Moldova'
+SEARCH_ELEMENT = 'Audi'
 HOW_MANY_ELEMENTS = 10
 AGREE_BUTTON = (By.ID, 'L2AGLb')
 SEARCH_BOX = (By.NAME, 'q')
 NEXT_PAGE = (By.ID, 'pnnext')
 SEARCHED_ELEMENT = (By.XPATH, '//h3[@class = "LC20lb MBeuO DKV0Md"]')
-
-driver = webdriver.Chrome('..\\part_2_automation\\drivers\\chromedriver.exe')
-driver.implicitly_wait(5)
-driver.maximize_window()
-driver.get('https://www.google.com')
-driver.find_element(*AGREE_BUTTON).click()
-driver.find_element(*SEARCH_BOX).send_keys(SEARCH_ELEMENT, Keys.RETURN)
-
-search_list = []
-while len(search_list) <= HOW_MANY_ELEMENTS:
-    search_elements = driver.find_elements(*SEARCHED_ELEMENT)
-
-    for element in search_elements:
-        search_list.append(element.text)
-
-    driver.find_element(*NEXT_PAGE).click()
-
-# print(search_list)
-
-positive_result = 0
-negative_result = 0
-for item in search_list:
-
-    if SEARCH_ELEMENT in item:
-        positive_result += 1
-    elif SEARCH_ELEMENT not in item:
-        negative_result += 1
-
-print(
-    f'\n********There are {positive_result} results with {SEARCH_ELEMENT} and {negative_result} without {SEARCH_ELEMENT}********\n')
+DRIVER = webdriver.Chrome('..\\part_2_automation\\drivers\\chromedriver.exe')
+LINK = 'https://www.google.com'
 
 
-driver.quit()
+def setup(driver, link):
+    driver.implicitly_wait(5)
+    driver.maximize_window()
+    driver.get(link)
+    driver.find_element(*AGREE_BUTTON).click()
+    driver.find_element(*SEARCH_BOX).send_keys(SEARCH_ELEMENT, Keys.RETURN)
+
+
+def get_elements(driver):
+    search_list = []
+    while len(search_list) <= HOW_MANY_ELEMENTS:
+        search_elements = driver.find_elements(*SEARCHED_ELEMENT)
+
+        for element in search_elements:
+            search_list.append(element.text)
+
+        driver.find_element(*NEXT_PAGE).click()
+        return search_list
+
+
+def check_the_result(search_list):
+    positive_result = 0
+    negative_result = 0
+    for item in search_list:
+
+        if SEARCH_ELEMENT in item:
+            positive_result += 1
+        elif SEARCH_ELEMENT not in item:
+            negative_result += 1
+
+    print(f'There are {positive_result} results with {SEARCH_ELEMENT} and {negative_result} without {SEARCH_ELEMENT}')
+
+
+def teardown(driver):
+    driver.quit()
+
+
+def main():
+    setup(driver=DRIVER, link=LINK)
+    my_list = get_elements(driver=DRIVER)
+    check_the_result(search_list=my_list)
+    teardown(driver=DRIVER)
+
+
+if __name__ == '__main__':
+    main()
